c0ae6ce566c507159186a0f6065d06ed88cd64fa
Michele Silvestri <mic.silvestri73@gmail.com>
2017-03-13 16:29:57 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup, Extension
+ 
+module1 = Extension('blake2',
+                sources = ['blake2/blake2module.c','blake2/blake2b-ref.c'],
+                include_dirs = ['blake2'],
+                extra_compile_args = ["-std=c99", "-Wall", "-pedantic"] )
+
+ 
+setup (name = 'blake2',
+        packages=['blake2'],
+        version = '0.1.4',
+        ext_modules = [module1],
+        description = 'blake2 hash function module for python',
+        license='Public Domain',
+        author='Bae KwonHan',
+        author_email='darjeeling@gmail.com',
+        maintainer='Bae KwonHan',
+        maintainer_email='darjeeling@gmail.com',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+            'Operating System :: POSIX',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: Implementation :: CPython',
+            'Topic :: Security :: Cryptography'
+          ],
+        )
